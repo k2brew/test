1c561e1f75adecae00679c3c2c52dc66f2ee0e53
brad kaus <brad.kaus@gmail.com>
2017-01-20 19:52:15 -0500

    Whitespace example.

--- script.py
+++ script.py
@@ -1,13 +1,5 @@
-# my_int is set to 7 below. What do you think
-# will happen if we reset it to 3 and print the result?
+def spam():
+  eggs = 12
+  return eggs
 
-my_int = 7
-
-# Change the value of my_int to 3 on line 8!
-
-my_int = 3
-
-# Here's some code that will print my_int to the console:
-# The print keyword will be covered in detail soon!
-
-print(my_int)
+print(spam())
