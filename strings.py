4f6472ea26e682f966a5eb7ded52ef383b786c6b
brad kaus <brad.kaus@gmail.com>
2017-01-20 22:02:17 -0500

    initial release

--- strings.py
+++ strings.py
@@ -8,3 +8,49 @@ viking = "Teresa"
 print(caesar)
 print(praline)
 print(viking)
+
+'''
+The string "PYTHON" has six characters,
+numbered 0 to 5, as shown below:
+
++---+---+---+---+---+---+
+| P | Y | T | H | O | N |
++---+---+---+---+---+---+
+  0   1   2   3   4   5
+
+So if you wanted "Y", you could just type
+"PYTHON"[1] (always start counting from 0!)
+'''
+
+fifth_letter = "MONTY"[4]
+print(fifth_letter)
+
+parrot = "Norwegian Blue"
+print(len(parrot))
+print(parrot.lower())
+print(parrot.upper())
+
+"""Declare and assign your variable on line 4,
+then call your method on line 5!"""
+
+pi = 3.14
+print(str(pi))
+
+ministry = "The Ministry of Silly Walks"
+
+print(len(ministry))
+print(ministry.upper())
+
+"""Assign the string "Ping!" to
+the variable the_machine_goes on
+line 5, then print it out on line 6!"""
+
+the_machine_goes = "Ping!"
+print(the_machine_goes)
+
+print("The value of pi is around " + str(3.14))
+
+string_1 = "Camelot"
+string_2 = "place"
+
+print("Let's not go to %s. 'Tis a silly %s." % (string_1, string_2))
